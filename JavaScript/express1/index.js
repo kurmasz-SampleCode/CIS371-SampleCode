56e61340782755b5cf300d243e0f8410514e0b6e
Zachary Kurmas <kurmasz@gvsu.edu>
2020-02-10 17:09:25 -0500

    Added more express examples.

--- JavaScript/express1/index.js
+++ JavaScript/express1/index.js
@@ -1,3 +1,5 @@
+// Express documentation: https://expressjs.com/en/api.html
+
 
 /* Import the express npm module */
 const express = require('express')
@@ -8,7 +10,7 @@ const port = 3000
 
 
 /* Tell the server to use EJS by default */
-app.set('view engine',	'ejs');
+app.set('view engine', 'ejs');
 
 
 /* Sample route returning a simple, static message. */
@@ -26,8 +28,18 @@ app.get('/sayHi', (req, res) => {
 
 /* Sample route returning a rendered EJS view */
 app.get('/testView', (req, res) => {
-    res.render('firstView', {name: req.query.name});
-    
+    // By default, Express looks for views in the `views` directory.
+    res.render('firstView', { name: req.query.name });
+
+});
+
+/* Sample route returning a static HTML file. */
+app.get('/staticHTML', (req, res) => {
+    // When using sendFile, you either need to 
+    // (1) Provide an absolute path name, or 
+    // (2) Provide a "root" parameter.  
+    // __dirname provide the absolute path of the currently executing file.
+    res.sendFile('hello.html', { root: __dirname });
 });
 
 /* Launch the server */
