9e1b866ac0aebcdea3c38beb93e41ce19f203907
Zachary Kurmas <kurmasz@gvsu.edu>
2022-11-08 15:27:46 -0500

    Added artificial delay to the /colors route

--- ColorListAPI/index.js
+++ ColorListAPI/index.js
@@ -1,3 +1,4 @@
+const { query } = require('express')
 const express = require('express')
 const ColorDB = require('./ColorDB')
 
@@ -10,14 +11,20 @@ app.dont_use = f => f
 // This sets a CORS header.  It allows requests from JS 
 // that didn't originate here
 // !!!!! Don't ever use "*" in production!!!
-app.dont_use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     next();
 });
 
 
 app.get('/colors', async (req, res) => {
-    res.json(await ColorDB.allColors())
+    // Introduce an artificial delay so user can see the effects of loading.    
+    let delay = 500;
+    if (req.query.hasOwnProperty('delay')) {
+        delay = req.query.delay;
+        console.log("Using a delay of " + delay);
+    }
+    setTimeout( async () => res.json(await ColorDB.allColors()), delay)
 })
 
 /* Launch the server */
