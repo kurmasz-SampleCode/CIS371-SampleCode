08d7158b38715d7d44a5cbfa59222f568290c04a
Zachary Kurmas <kurmasz@gvsu.edu>
2023-03-16 11:45:40 -0400

    Add Dog

--- JavaScriptBasics/Dog.js
+++ JavaScriptBasics/Dog.js
@@ -1,48 +1,48 @@
-"use strict";
+'use strict'
 
 /**
  * Created by kurmasz on 2/10/15.
  */
-var Dog = (function () {
+let Dog = (function () {
 
-    var dogCount = 0;
-    var incrementDogCount = function () {
-        dogCount++;
+    let dogCount = 0
+    let incrementDogCount = function () {
+        dogCount++
     }
-    var numDogs = function () {
-        return dogCount;
+    let numDogs = function () {
+        return dogCount
     }
 
 
     // constructor
-    var init = function (name_in, height_in, weight_in) {
+    let init = function (name_in, height_in, weight_in) {
 
-        var name = name_in;
-        var height = height_in;
-        var weight = weight_in;
-
-
-        var speak = function () {
-            return "Woof";
-        };
+        let name = name_in
+        let height = height_in
+        let weight = weight_in
 
 
-        var toString = function () {
+        let speak = function () {
+            return 'Woof'
+        }
+
+
+        let toString = function () {
             // Notice the removal of "this"
-            return name + " has height " + height + " and weight " + weight
-        };
+            return name + ' has height ' + height + ' and weight ' + weight
+        }
 
         // Notice calcBMI must be moved inside makeDog to have
         // access to private data.
-        var calcBMI = function () {
-            return weight / (height * height);
-        };
+        let calcBMI = function () {
+            return weight / (height * height)
+        }
 
-        var setHeight = function (newHeight) {
-            height = newHeight;
-        };
+        let setHeight = function (newHeight) {
+            height = newHeight
+        }
 
-        incrementDogCount();
+        incrementDogCount()
 
         // If you don't add a function to the list below, it is effectively private.
         return {
@@ -51,11 +51,11 @@ var Dog = (function () {
             bmi: calcBMI,
             setHeight: setHeight
         }
-    }; // end of init
+    } // end of init
 
     return {
         numDogs: numDogs,
         init: init
     }
-})();
+})()
 
