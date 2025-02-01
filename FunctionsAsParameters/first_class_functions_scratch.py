70641b1d65add6ed6aa0ddc5e9d71db99616608f
Zachary Kurmas <kurmasz@gvsu.edu>
2025-02-01 10:38:59 -0500

    Tweaked demo code for video

--- FunctionsAsParameters/first_class_functions_scratch.py
+++ FunctionsAsParameters/first_class_functions_scratch.py
@@ -2,7 +2,7 @@
 students = [
     {"first_name": "Ella", "last_name": "Smith", "gpa": 3.6, "credit_hours": 103},
     {"first_name": "Liam", "last_name": "Johnson", "gpa": 2.9, "credit_hours": 60},
-    {"first_name": "Noah", "last_name": "Williams", "gpa": 3.8, "credit_hours": 30},
+    {"first_name": "Noah", "last_name": "Williams", "gpa": 3.8, "credit_hours": 15},
     {"first_name": "Olivia", "last_name": "Brown", "gpa": 3.2, "credit_hours": 75},
     {"first_name": "Emma", "last_name": "Jones", "gpa": 3.4, "credit_hours": 93},
     {"first_name": "Ava", "last_name": "Garcia", "gpa": 1.7, "credit_hours": 50},
@@ -18,7 +18,7 @@ students = [
 #
 def filter_for_probation(students, threshold):
     answer = []
-    for s in students:
+    for s in students: 
         if s['gpa'] < threshold:
             answer.append(s)
     return answer
@@ -30,26 +30,28 @@ def filter_by_credit_hours(students, low, high):
             answer.append(s)
     return answer
 
-
-def filter(the_list, the_filter):
+#
+# If we can pass code as a parameter, we can abstract the 
+# two functions above:
+#
+def filter(students, filter_condition):
     answer = []
-    for item in the_list:
-        if the_filter(item):
-            answer.append(item)
+    for s in students: 
+        if filter_condition(s):
+            answer.append(s)
     return answer
 
+def on_probation(s):
+    return s['gpa'] < 2.0
 
-def is_senior(student):
-    return student['credit_hours'] >= 90
-
-def on_probation(student):
-    return student['gpa'] < 2.0
-
-seniors = filter(students, is_senior)
-print(f"Seniors: {seniors}")
+def is_senior(s):
+    return s['credit_hours'] >= 90
 
 to_warn = filter(students, on_probation)
 print(f"On probation: {to_warn}")
+
+seniors = filter(students, is_senior)
+print(f"Seniors: {seniors}")
 
 #
 # So far, so good.  But, notice that we are writing an entire function that 
@@ -57,30 +59,36 @@ print(f"On probation: {to_warn}")
 #
 # Python allows you to create anonymous functions. These are called "lambdas"
 #
-seniors2 = filter(students, lambda s : s['credit_hours'] >= 90)
-print(f"Seniors 2: {seniors2}")
+to_warn = filter(students, lambda s : s['gpa'] < 2.0)
+print(f"On probation: {to_warn}")
 
-to_warn2 = filter(students, lambda s : s['gpa'] < 2.0)
-print(f"To Warn 2: {to_warn2}")
+seniors = filter(students, lambda s : s['credit_hours'] >= 90)
+print(f"Seniors: {seniors}")
+
+juniors = filter(students, lambda s : s['credit_hours'] >= 60 and s['credit_hours'] < 90)
+print(f"Juniors: {juniors}")
+
+# In python, lambdas are limited to a single expression
+# This is not true for most programming languages
 
 #
-# In python, lambdas are limited to a single expression.
-# This is not true for most programming languages.
-#
-
-
-#
-# Closures
+# Closure 
 #
 # You can use the idea of a closure to build more complex filters.
 # The "make_gpa_filter" function "closes" around the threshold 
 # variable allowing you to effectively use it inside filter
 
-def make_gpa_filter(threshold):
-    return lambda s : s['gpa'] >= threshold
 
-above_3 = filter(students, make_gpa_filter(3.0))
-print(f"Students with 3.0 or better: {above_3}")
+def make_credit_hour_filter(low, high):
+    return lambda s : s['credit_hours'] >= low and s['credit_hours'] < high
 
-above_35 = filter(students, make_gpa_filter(3.5))
-print(f"Students with 3.5 or better: {above_35}")
+freshmen = filter(students, make_credit_hour_filter(0, 30))
+sophomore = filter(students, make_credit_hour_filter(30, 60))
+junior = filter(students, make_credit_hour_filter(60, 90))
+senior = filter(students, make_credit_hour_filter(90, 10000))
+
+print(f"Freshmen {freshmen}")
+print(f"Sophomore {sophomore}")
+print(f"Junior {junior}")
+print(f"Senior {senior}")
+
